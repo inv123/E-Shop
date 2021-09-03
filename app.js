7c56a27ee23d92913e35d4deb71af4a19ce68584
Nedyalko Tiholov <nedyalko.tiholov@yahoo.com>
2021-09-03 20:56:04 +0300

    Updates main JS

--- app.js
+++ app.js
@@ -1,18 +1,22 @@
 import page from './node_modules/page/page.mjs';
+import middleware from './middleware.js';
 
-let navCont = undefined;
-let viewCont = undefined;
-let sideBannerConnt = undefined;
-let smallBannerSection = undefined;
-let trendingItemContainer = undefined;
-let mediumBannerContainer = undefined;
-let hotItemContainer = undefined;
-let yellowBannerContainer = undefined;
-let onSaleBestSaleTopViewContainer = undefined;
-let blogSectionContainer = undefined;
-let shopServiceContainer = undefined;
-let newsLetterContainer = undefined;
-let modal = undefined;
+let navCont = document.querySelector('header');
+let sideBannerConnt = document.querySelector('.hero-slider');
+let smallBannerSection = document.querySelector('.small-banner');
+let trendingItemContainer = document.querySelector('.product-area');
+let mediumBannerContainer = document.querySelector('.medium-banner');
+let hotItemContainer = document.querySelector('.most-popular');
+let yellowBannerContainer = document.querySelector('.free-version-banner');
+let onSaleBestSaleTopViewContainer = document.querySelector('.shop-home-list');
+let blogSectionContainer = document.querySelector('.shop-blog');
+let shopServiceContainer = document.querySelector('.shop-services');
+let newsLetterContainer = document.querySelector('.shop-newsletter');
+let modal = document.querySelector('#exampleModal');
+
+middleware.initialize(navCont, sideBannerConnt,smallBannerSection, trendingItemContainer, mediumBannerContainer,hotItemContainer, yellowBannerContainer,onSaleBestSaleTopViewContainer,blogSectionContainer, shopServiceContainer, newsLetterContainer, modal);
+
+page('/', middleware.decorateContext)
 
 page.start();
 
