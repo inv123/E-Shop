758e1e0288e8b0aecc7b786c6538644d1264cf14
Nedyalko Tiholov <nedyalko.tiholov@yahoo.com>
2021-09-03 20:21:57 +0300

    Update middlware

--- middleware.js
+++ middleware.js
@@ -2,10 +2,34 @@ import {render} from './node_modules/lit-html/lit-html.js';
 
 let navCont = undefined;
 let viewCont = undefined;
+let sideBannerConnt = undefined;
+let smallBannerSection = undefined;
+let trendingItemContainer = undefined;
+let mediumBannerContainer = undefined;
 
-function initialize(navElement, viewElement){
+function initialize(navElement, viewElement, sideBannerElement, smallBannerElement, trendingItemElement, mediumBannerElement){
     navCont = navElement;
     viewCont = viewElement;
+    sideBannerConnt = sideBannerElement;
+    smallBannerSection = smallBannerElement;
+    trendingItemContainer = trendingItemElement;
+    mediumBannerContainer = mediumBannerElement;
+}
+
+async function renderMediumBanner(template){
+    render(template, mediumBannerContainer)
+}
+
+async function renderTrendingItem(template){
+    render(template, trendingItemContainer)
+}
+
+async function renderSmallBanner(template){
+    render(template, smallBannerSection)
+}
+
+async function renderSideBanner(template){
+    render(template, sideBannerConnt)
 }
 
 async function renderNav(template){
@@ -19,6 +43,10 @@ async function renderView(template){
 function decorateContext(context, next){
     context.renderNav = renderNav;
     context.renderView = renderView;
+    context.renderSideBanner = renderSideBanner;
+    context.renderSmallBanner = renderSmallBanner;
+    context.renderTrendingItem = renderTrendingItem;
+    context.renderMediumBanner = renderMediumBanner;
 
     next();
 }
@@ -27,5 +55,9 @@ export default {
     initialize, 
     renderNav,
     renderView,
+    renderSideBanner,
+    renderSmallBanner,
+    renderTrendingItem,
+    renderMediumBanner,
     decorateContext
 }
