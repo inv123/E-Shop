900f3f0917ee0f0a8d919231ed585f1eda1e5090
Nedyalko Tiholov <nedyalko.tiholov@yahoo.com>
2021-09-03 20:39:58 +0300

    Add all middlewares

--- middleware.js
+++ middleware.js
@@ -6,14 +6,56 @@ let sideBannerConnt = undefined;
 let smallBannerSection = undefined;
 let trendingItemContainer = undefined;
 let mediumBannerContainer = undefined;
+let hotItemContainer = undefined;
+let yellowBannerContainer = undefined;
+let onSaleBestSaleTopViewContainer = undefined;
+let blogSectionContainer = undefined;
+let shopServiceContainer = undefined;
+let newsLetterContainer = undefined;
+let modal = undefined;
 
-function initialize(navElement, viewElement, sideBannerElement, smallBannerElement, trendingItemElement, mediumBannerElement){
+function initialize(navElement, viewElement, sideBannerElement, smallBannerElement, trendingItemElement, mediumBannerElement, hotItemElement, yellowBannerElement,onSaleBestSaleTopViewElement, blogElement, shopServiceElement, newsLetterElement, modalElement){
     navCont = navElement;
     viewCont = viewElement;
     sideBannerConnt = sideBannerElement;
     smallBannerSection = smallBannerElement;
     trendingItemContainer = trendingItemElement;
     mediumBannerContainer = mediumBannerElement;
+    hotItemContainer = hotItemElement;
+    yellowBannerContainer = yellowBannerElement;
+    onSaleBestSaleTopViewContainer = onSaleBestSaleTopViewElement;
+    blogSectionContainer = blogElement;
+    shopServiceContainer = shopServiceElement;
+    newsLetterContainer = newsLetterElement;
+    modal = modalElement;
+}
+
+async function renderModal(template){
+    render(template, modal)
+}
+
+async function renderNewsLetter(template){
+    render(template, newsLetterContainer)
+}
+
+async function renderShopService(template){
+    render(template, shopServiceContainer)
+}
+
+async function renderBlog(template){
+    render(template, blogSectionContainer)
+}
+
+async function renderonSaleBestSaleTopView(template){
+    render(template, onSaleBestSaleTopViewContainer)
+}
+
+async function renderYellowItem(template){
+    render(template, yellowBannerContainer)
+}
+
+async function renderhotItem(template){
+    render(template, hotItemContainer)
 }
 
 async function renderMediumBanner(template){
@@ -47,6 +89,13 @@ function decorateContext(context, next){
     context.renderSmallBanner = renderSmallBanner;
     context.renderTrendingItem = renderTrendingItem;
     context.renderMediumBanner = renderMediumBanner;
+    context.renderhotItem = renderhotItem;
+    context.renderYellowItem = renderYellowItem;
+    context.renderonSaleBestSaleTopView = renderonSaleBestSaleTopView;
+    context.renderBlog = renderBlog;
+    context.renderShopService = renderShopService;
+    context.renderNewsLetter = renderNewsLetter;
+    context.renderModal = renderModal;
 
     next();
 }
@@ -59,5 +108,12 @@ export default {
     renderSmallBanner,
     renderTrendingItem,
     renderMediumBanner,
+    renderhotItem,
+    renderYellowItem,
+    renderonSaleBestSaleTopView,
+    renderBlog,
+    renderShopService,
+    renderNewsLetter,
+    renderModal,
     decorateContext
 }
