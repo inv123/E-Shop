1520cc82448fd94d02f208af885d740b2ea3b1f3
Nedyalko Tiholov <nedyalko.tiholov@yahoo.com>
2021-09-04 12:20:37 +0300

    Fix homepage folders name

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
 import page from './node_modules/page/page.mjs';
 import middleware from './middleware.js';
 import nav from './nav/nav.js';
-import sideBannerElement from './pages/homepage/1.sideBanner/sideBannerElement.js.js';
-import smallBanner from './pages/homepage/smallBanner/smallBanner.js';
-import trendingItem from './pages/homepage/trendingItem/trendingItem.js';
-import mediumBanner from './pages/homepage/4.mediumBanner/mediumBanner.js.js'
-import hotItems from './pages/homepage/hotItems/hotItems.js';
-import yellowBanner from './pages/homepage/yellowBanner/yellowBanner.js'
-import onSaleBestSale from './pages/homepage/onSaleBestSale/onSaleBestSale.js';
-import blogSection from './pages/homepage/blogSection/blogSection.js';
-import shopService from './pages/homepage/shopService/shopService.js';
-import newsLetter from './pages/homepage/newsLetter/newsLetter.js';
+import sideBannerElement from './pages/homepage/1.sideBanner/sideBannerElement.js';
+import smallBanner from './pages/homepage/2.smallBanner/smallBanner.js';
+import trendingItem from './pages/homepage/3.trendingItem/trendingItem.js';
+import mediumBanner from './pages/homepage/4.mediumBanner/mediumBanner.js'
+import hotItems from './pages/homepage/5.hotItems/hotItems.js';
+import yellowBanner from './pages/homepage/6.yellowBanner/yellowBanner.js'
+import onSaleBestSale from './pages/homepage/7.onSaleBestSale/onSaleBestSale.js';
+import blogSection from './pages/homepage/8.blogSection/blogSection.js';
+import shopService from './pages/homepage/9.shopService/shopService.js';
+import newsLetter from './pages/homepage/10.newsLetter/newsLetter.js';
 import modal from './modal/modal.js'
 
 let navCont = document.querySelector('header');
@@ -24,7 +24,7 @@ let onSaleBestSaleTopViewContainer = document.querySelector('.shop-home-list');
 let blogSectionContainer = document.querySelector('.shop-blog');
 let shopServiceContainer = document.querySelector('.shop-services');
 let newsLetterContainer = document.querySelector('.shop-newsletter');
-let modalElement = document.querySelector('.modal');
+let modalElement = document.querySelector('#exampleModal');
 
 middleware.initialize(navCont, sideBannerConnt,smallBannerSection, trendingItemContainer, mediumBannerContainer,hotItemContainer, yellowBannerContainer,onSaleBestSaleTopViewContainer,blogSectionContainer, shopServiceContainer, newsLetterContainer, modalElement);
 
