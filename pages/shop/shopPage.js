8e84fdd292f4f9f0e984e17c46885d665ff8b3a8
Nedyalko Tiholov <nedyalko.tiholov@yahoo.com>
2021-09-15 19:42:46 +0300

    Shop Filter in progress

--- pages/shop/shopPage.js
+++ pages/shop/shopPage.js
@@ -4,22 +4,23 @@ import {shopTemplate } from "./shopTemplate.js";
 async function getView(context){
     
     let products = await productServices.getAllProducts();
+    let key = decodeURIComponent(context.path).split('=')[1];
+    let value = decodeURIComponent(context.path).split('=')[2];
     
     if(context.querystring){
         
-        let key = decodeURIComponent(context.path).split('=')[1];
-        let value = decodeURIComponent(context.path).split('=')[2];
+       
 
         console.log(key);
         console.log(value);
 
         if(key === 'price'){
             if(value === '1-50'){
-                context.renderView(shopTemplate(Object.values(products).filter(x => x[key] <= 50)));
+                context.renderView(shopTemplate(Object.values(products).filter(x => x.price <= 50.00)));
             }else if(value === '51-100'){
-                context.renderView(shopTemplate(Object.values(products).filter(x => x[key] > 50 && x[key] <= 100)));
+                context.renderView(shopTemplate(Object.values(products).filter(x => x.price > 50 && x.price <= 100)));
             }else if(value === '101-199'){
-                context.renderView(shopTemplate(Object.values(products).filter(x => x[key] > 100 && x[key] <= 199)));
+                context.renderView(shopTemplate(Object.values(products).filter(x => x.price > 100 && x.price <= 199)));
             }
         }
 
