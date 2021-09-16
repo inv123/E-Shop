a32f3bd2189aa73126c0b155a2f1a2d7d1eeeada
Nedyalko Tiholov <nedyalko.tiholov@yahoo.com>
2021-09-16 08:33:23 +0300

    Fix shop page filter

--- pages/shop/shopPage.js
+++ pages/shop/shopPage.js
@@ -1,36 +1,35 @@
 import productServices from "../../service/productServices.js";
-import {shopTemplate } from "./shopTemplate.js";
+import {
+    shopTemplate
+} from "./shopTemplate.js";
 
-async function getView(context){
-    
+async function getView(context) {
+
     let products = await productServices.getAllProducts();
     let key = decodeURIComponent(context.path).split('=')[1];
     let value = decodeURIComponent(context.path).split('=')[2];
-    
-    if(context.querystring){
-        
-       
+    console.log(key);
+    if (key) {
 
-        console.log(key);
-        console.log(value);
-
-        if(key === 'price'){
-            if(value === '1-50'){
+        if (key === 'price') {
+            if (value === '1-50') {
                 context.renderView(shopTemplate(Object.values(products).filter(x => x.price <= 50.00)));
-            }else if(value === '51-100'){
+            } else if (value === '51-100') {
                 context.renderView(shopTemplate(Object.values(products).filter(x => x.price > 50 && x.price <= 100)));
-            }else if(value === '101-199'){
+            } else if (value === '101-199') {
                 context.renderView(shopTemplate(Object.values(products).filter(x => x.price > 100 && x.price <= 199)));
             }
+        } else {
+            context.renderView(shopTemplate(Object.values(products).filter(x => x.title === key)))
         }
 
-        context.renderView(shopTemplate(Object.values(products).filter(x => x.title === key)))
-    }else{
+
+    } else {
         context.renderView(shopTemplate(Object.values(products)))
     }
 
     console.log(context);
-    
+
 }
 export default {
     getView
